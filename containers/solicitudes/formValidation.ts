8f18acc452a05dc1adb23f7bc5dd2090715ecd6c
Israel López Tiscareño <63254555+israellopeztiscareno@users.noreply.github.com>
2025-09-30 12:43:04 -0600

    Crear piid random filled fields

--- containers/solicitudes/formValidation.ts
+++ containers/solicitudes/formValidation.ts
@@ -1,5 +1,6 @@
 // 📦 Dependencies
 import * as Yup from "yup";
+import ShortUUID from "short-uuid";
 
 // 🧾 Types
 import { formSolicitud } from "@/types/solicitudes/solicitud";
@@ -9,9 +10,11 @@ import {
   allowedPdfMimeTypes,
   fileMaxSize,
 } from "@/constants/file-validation";
+
 /*
  **Mensajes genérico
  */
+
 export const requiredMessage: string = "Este es un campo requerido";
 export const invalidFormatValidation: string = "Formato inválido";
 
@@ -19,15 +22,37 @@ export const RGX_LETTERS_NUMBERS = /^[a-zA-Z\d]+$/;
 export const RGX_LETTERS = /^[a-zA-Z]+$/;
 export const RGX_ONLY_NUMBERS = /^\d+$/;
 
+const digitsOnly = '0123456789';
+const decimalTranslator = ShortUUID(digitsOnly)
+const [id_material, tipo_material] = decimalTranslator.uuid().split("-")
+
+const randomAmount: number = (Math.random() * 10000) + 0.01;
+const amount = Math.round(randomAmount * 100) / 100;
+
+const descripciones = [
+  "Jabón Líquido para Manos Dove — 150ml",
+  "Jabón en Barra Lux — Rosas Francesas 125g",
+  "Shampoo Sedal Restauración Instantánea — 340ml",
+  "Desodorante Axe Body Spray Dark Temptation — 150ml",
+  "Mayonesa Hellmann’s Clásica — 390g",
+  "Crema Corporal Vaseline Cocoa Glow — 400ml",
+  "Limpiador CIF Crema Multiuso — 500ml",
+  "Desodorante Dove Roll-On Original — 50ml"
+]
+
+const getRandomDescription = () => {
+  const index = Math.floor(Math.random() * descripciones.length);
+  return descripciones[index];
+};
+
+const descripcion = getRandomDescription();
+
+
 export const solicitudSchema = Yup.object({
-  idMaterial: Yup.string()
-    .matches(RGX_ONLY_NUMBERS, invalidFormatValidation)
-    .required(requiredMessage),
+  idMaterial: Yup.string().required(requiredMessage),
   descripcion: Yup.string().required(requiredMessage),
   piezas: Yup.number().min(1, requiredMessage).required(requiredMessage),
-  tipoMaterial: Yup.string()
-    .matches(RGX_LETTERS_NUMBERS, invalidFormatValidation)
-    .required(requiredMessage),
+  tipoMaterial: Yup.string().required(requiredMessage),
   moneda: Yup.string()
     .matches(RGX_LETTERS, invalidFormatValidation)
     .required(requiredMessage),
@@ -36,13 +61,13 @@ export const solicitudSchema = Yup.object({
 });
 
 export const solicitudInitialValues: formSolicitud = {
-  idMaterial: "",
-  descripcion: "",
-  piezas: null,
-  tipoMaterial: "",
-  moneda: "",
-  precio: null,
-  canal: "",
+  idMaterial: `MFN-${id_material.toUpperCase()}`,
+  descripcion,
+  piezas: Math.floor(Math.random() * 100) + 1,
+  tipoMaterial: `AAX-${tipo_material.toUpperCase()}`,
+  moneda: "MXN",
+  precio: amount,
+  canal: "Min",
 };
 
 export const schema = Yup.object().shape({
